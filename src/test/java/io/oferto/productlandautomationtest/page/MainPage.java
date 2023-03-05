a0b23aa4ddbb5e178ecd00811994a1c19853e592
Miguel Salinas Gancedo <masalinas.gancedo@gmail.com>
2023-03-05 20:45:50 +0100

    last commit

--- src/test/java/io/oferto/productlandautomationtest/page/MainPage.java
+++ src/test/java/io/oferto/productlandautomationtest/page/MainPage.java
@@ -1,10 +1,14 @@
 package io.oferto.productlandautomationtest.page;
 
+import javax.annotation.PostConstruct;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 //@Component
 public class MainPage {
